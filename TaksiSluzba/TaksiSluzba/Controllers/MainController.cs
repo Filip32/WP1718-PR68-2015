08aca57bef5180ea6dae93f03ace6484f2833f77
filip32 <filipovic.filip32@gmail.com>
2018-06-09 22:39:28 +0200

    Created html client and MainController

---
+++ TaksiSluzba/TaksiSluzba/Controllers/MainController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace TaksiSluzba.Controllers
+{
+    public class MainController : ApiController
+    {
+        [HttpGet, Route("")]
+        public RedirectResult Index()
+        {
+            var requestUri = Request.RequestUri;
+            return Redirect(requestUri.AbsoluteUri + "Content/Main.html");
+        }
+
+
+
+    }
+}
